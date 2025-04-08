0ee792368e3b7eaf7b87e5bd8fd7b40ffc6b82a1
kavinwkp <1817136760@qq.com>
2025-04-08 21:47:57 +0800

    eval_moe

---
+++ lotus/script/eval_moe_libero_goal.sh
@@ -0,0 +1,31 @@
+ seed=1
+
+ python lotus/lifelong/evaluate_moe.py --benchmark libero_goal --algo moe --policy bc_transformer_moe_policy --seed "$seed" \
+ --experiment_dir experiments/libero_goal/MoeTask/BCTransformerMoEPolicy_seed1/libero_goal_exp10_run_001 --ep 50 --task_id 0
+
+ python lotus/lifelong/evaluate_moe.py --benchmark libero_goal --algo moe --policy bc_transformer_moe_policy --seed "$seed" \
+ --experiment_dir experiments/libero_goal/MoeTask/BCTransformerMoEPolicy_seed1/libero_goal_exp10_run_001 --ep 50 --task_id 1
+
+ python lotus/lifelong/evaluate_moe.py --benchmark libero_goal --algo moe --policy bc_transformer_moe_policy --seed "$seed" \
+ --experiment_dir experiments/libero_goal/MoeTask/BCTransformerMoEPolicy_seed1/libero_goal_exp10_run_001 --ep 50 --task_id 2
+
+ python lotus/lifelong/evaluate_moe.py --benchmark libero_goal --algo moe --policy bc_transformer_moe_policy --seed "$seed" \
+ --experiment_dir experiments/libero_goal/MoeTask/BCTransformerMoEPolicy_seed1/libero_goal_exp10_run_001 --ep 50 --task_id 3
+
+ python lotus/lifelong/evaluate_moe.py --benchmark libero_goal --algo moe --policy bc_transformer_moe_policy --seed "$seed" \
+ --experiment_dir experiments/libero_goal/MoeTask/BCTransformerMoEPolicy_seed1/libero_goal_exp10_run_001 --ep 50 --task_id 4
+
+  python lotus/lifelong/evaluate_moe.py --benchmark libero_goal --algo moe --policy bc_transformer_moe_policy --seed "$seed" \
+ --experiment_dir experiments/libero_goal/MoeTask/BCTransformerMoEPolicy_seed1/libero_goal_exp10_run_001 --ep 50 --task_id 5
+
+ python lotus/lifelong/evaluate_moe.py --benchmark libero_goal --algo moe --policy bc_transformer_moe_policy --seed "$seed" \
+ --experiment_dir experiments/libero_goal/MoeTask/BCTransformerMoEPolicy_seed1/libero_goal_exp10_run_001 --ep 50 --task_id 6
+
+ python lotus/lifelong/evaluate_moe.py --benchmark libero_goal --algo moe --policy bc_transformer_moe_policy --seed "$seed" \
+ --experiment_dir experiments/libero_goal/MoeTask/BCTransformerMoEPolicy_seed1/libero_goal_exp10_run_001 --ep 50 --task_id 7
+
+ python lotus/lifelong/evaluate_moe.py --benchmark libero_goal --algo moe --policy bc_transformer_moe_policy --seed "$seed" \
+ --experiment_dir experiments/libero_goal/MoeTask/BCTransformerMoEPolicy_seed1/libero_goal_exp10_run_001 --ep 50 --task_id 8
+
+ python lotus/lifelong/evaluate_moe.py --benchmark libero_goal --algo moe --policy bc_transformer_moe_policy --seed "$seed" \
+ --experiment_dir experiments/libero_goal/MoeTask/BCTransformerMoEPolicy_seed1/libero_goal_exp10_run_001 --ep 50 --task_id 9
